8801564cb08a12fad41ac0a9a760fa2704609be7
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-08-01 17:03:18 +0530

    fix: Patient onboarding

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,4 +1,4 @@
-// patient.actions.ts
+"use server";
 
 import { ID, Query } from "node-appwrite";
 import { users } from "../appwrite.config";
@@ -25,31 +25,3 @@ export const createUser = async (user: CreateUserParams) => {
     throw error;
   }
 };
-
-// CREATE APPWRITE USER
-// export const createUser = async (user: CreateUserParams) => {
-//   try {
-//     // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
-//     const newuser = await users.create(
-//       ID.unique(),
-//       user.email,
-//       user.phone,
-//       undefined,
-//       user.name
-//     );
-
-//     console.log({ newuser });
-//     // return parseStringify(newuser);
-//   } catch (error: any) {
-//     // Check existing user
-//     if (error && error?.code === 409) {
-//       const existingUser = await users.list([
-//         Query.equal("email", [user.email]),
-//       ]);
-//       // console.log(existingUser.users[0]);
-
-//       return existingUser.users[0];
-//     }
-//     console.error("An error occurred while creating a new user:", error);
-//   }
-// };
--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,26 +1,24 @@
 import * as sdk from "node-appwrite";
 
 export const {
+  NEXT_PUBLIC_ENDPOINT,
   PROJECT_ID,
   API_KEY,
   DATABASE_ID,
   PATIENT_COLLECTION_ID,
   DOCTOR_COLLECTION_ID,
   APPOINTMENT_COLLECTION_ID,
-  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
-  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+  NEXT_PUBLIC_BUCKET_ID,
 } = process.env;
 
 const client = new sdk.Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1")
-  .setProject("66a912180026e4c9bdfa")
-  .setKey(
-    "fa39ffb4db4c707ae285ace3f4c23e7881786ae1e2def7787336623089912dae735a8b3cd224463bf2b89e8ecb400d2179a448b0bfb6afd5ec9203619fb5cd066916c3e922fb27388f3531ff63017676b87b45562a767e8baadacb5a3fce7e916d8ce9a8618ce20d4b574d7796df58b3339e1752d04003e0893122f17fa9a9c9"
-  );
+  .setEndpoint(NEXT_PUBLIC_ENDPOINT!)
+  .setProject(PROJECT_ID!)
+  .setKey(API_KEY!);
 
 export const databases = new sdk.Databases(client);
-export const storage = new sdk.Storage(client);
-export const messaging = new sdk.Messaging(client);
 export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
